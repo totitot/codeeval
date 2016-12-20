461c4a9e7e008ede98d7e57e43ab89b096e7ffe2
totitot <kenth.crisolo@gmail.com>
2016-12-20 12:59:10 +0800

    tried to fix comb sort, still not working

--- moderate/meetcombsort.cpp
+++ moderate/meetcombsort.cpp
@@ -23,18 +23,29 @@ int main( int argc, char** argv ){
 		int distance = vi.size()-1;
 		unsigned int iter = 0;
 		do{
-			//cout << distance << endl;
+			#ifdef DEBUG
+				cout << "distance: " << distance << endl;
+			#endif
+
 			for( int i = 0, j = distance; j < vi.size(); i++, j++ ){
 				if( vi[i] > vi[j] ) swap(vi[i],vi[j]);
 			}
 			distance /= 1.25;
 			if( distance == 0 ) distance = 1;
 			iter++;
+
+			#ifdef DEBUG
+				cout << "# iter: " << iter << endl;
+				for( auto i : vi ) cout << i << " ";
+				cout << endl;
+			#endif
 		}
 		while( !is_sorted( vi.begin(), vi.end() ) );
-		cout << iter << endl;
-		//for( auto i : vi ) cout << i << " ";
-		//cout << endl;
+		#ifdef DEBUG
+			cout << endl;
+		#else
+			cout << iter << endl;
+		#endif
 	}
 
 }
